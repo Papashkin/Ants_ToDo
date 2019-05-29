ed49954c08ec1a2700b26bfb3be36461efa58c1f
Pavel Antoshkin <p.antoshkin@napoleonit.ru>
2019-05-29 10:04:25 +0300

    Code clean + updates in Gradle build

--- app/src/main/java/com/example/ants_todo/data/db/lists/ListsDao.kt
+++ app/src/main/java/com/example/ants_todo/data/db/lists/ListsDao.kt
@@ -10,17 +10,11 @@ interface ListsDao {
     @Query("SELECT * from lists")
     fun getAll(): LiveData<List<ListModel>>
 
-    @Query("Select * from lists where name = :name")
-    fun getByName(name: String): ListModel
-
     @Query("Select * from lists where id = :id")
     fun getById(id: Int): ListModel
 
     @Insert
     fun add(list: ListModel)
-
-    @Update
-    fun update(list: ListModel)
 
     @Delete
     fun delete(list: ListModel)
